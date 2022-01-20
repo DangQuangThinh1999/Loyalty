752ac885f5f4a08bcebd24cc02df27728392c5f5
THỊNH ĐẶNG QUANG <93868132+DangQuangThinh1999@users.noreply.github.com>
2022-01-20 22:53:50 +0700

    Add project files.

---
+++ Loyalty/Controllers/Admin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loyalty.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
